88d766b7bb9841954e39d8060169e1493d8f7414
kaw42 <kaw42@njit.edu>
2022-03-07 00:11:12 -0500

    changed base page copyright and test

--- tests/context_process_test.py
+++ tests/context_process_test.py
@@ -16,7 +16,7 @@ def test_context_variables_year(client):
     current_date_time = datetime.datetime.now()
     date = current_date_time.date()
     year = date.strftime("%Y")
-    test_string = f"Copyright: {year}"
+    test_string = f"Kelly Wu {year}"
     content = bytes(test_string, 'utf-8')
     assert response.status_code == 200
     assert content in response.data
